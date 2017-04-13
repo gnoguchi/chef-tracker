ff1d6a870a890fe2a179b12e7b5b8eb4b7707165
Gabriel Noguchi <gabriel.noguchi@soaexpert.com.br>
2017-04-13 15:51:00 -0300

    granvando alterações no firebase

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,11 +3,13 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions:{
     enter(cook){
-      Ember.set(cook,'isCookingToday',true)
+      Ember.set(cook,'isCookingToday',true);
+      cook.save();
     },
 
     exit(cook){
-      Ember.set(cook,'isCookingToday',false)
+      Ember.set(cook,'isCookingToday',false);
+      cook.save();
     }
   }
 
