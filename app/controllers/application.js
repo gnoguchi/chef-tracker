74ce9fc3f29ad708a325a5577b1a3c9bbaa32869
Gabriel Noguchi <gabriel.noguchi@soaexpert.com.br>
2017-04-17 18:50:18 -0300

    atualizações projeto

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  newItem:null,
+  chefLength: Ember.computed.alias('model.length'),
+  availableChefs: Ember.computed.filterBy('model','isCookingToday',true),
   actions:{
     enter(cook){
       Ember.set(cook,'isCookingToday',true);
@@ -10,6 +13,19 @@ export default Ember.Controller.extend({
     exit(cook){
       Ember.set(cook,'isCookingToday',false);
       cook.save();
+    },
+    saveChefItem(){
+      debugger;
+      this.store.createRecord('cook',{
+        isCookingToday: false,
+        name: this.get('newItem')
+      }).save()
+
+      this.set('newItem','')
+
+    },
+    fireCook(cook){
+      cook.destroyRecord();
     }
   }
 
