f6496244529bda394d21a4e18e4b652c52154300
Gabriel Noguchi <gabriel.noguchi@soaexpert.com.br>
2017-05-15 15:03:50 -0300

    atualizações

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,6 +4,8 @@ export default Ember.Controller.extend({
   newItem:null,
   chefLength: Ember.computed.alias('model.length'),
   availableChefs: Ember.computed.filterBy('model','isCookingToday',true),
+  chefStudents: Ember.computed.mapBy('model','students'),
+  totalStudents: Ember.computed.sum('chefStudents'),
   actions:{
     enter(cook){
       Ember.set(cook,'isCookingToday',true);
@@ -26,6 +28,16 @@ export default Ember.Controller.extend({
     },
     fireCook(cook){
       cook.destroyRecord();
+    },
+    addStudent(cook){
+      cook.set('students', cook.get('students') + 1);
+      cook.save();
+    },
+    removeStudent(cook){
+      if(cook.get('students') >0){
+      cook.decrementProperty('students');
+      cook.save();
+      }
     }
   }
 
--- app/models/cook.js
+++ app/models/cook.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     name: DS.attr('string'),
-    isCookingToday: DS.attr('boolean')
+    isCookingToday: DS.attr('boolean'),
+    students:DS.attr('number', {defaultValue: 0})
 });
